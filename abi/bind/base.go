0ff70a56437d59dd14c402862006c7deceb80d08
ningxin <292409083@qq.com>
2020-09-08 11:17:55 +0800

    modify aysc transactor

--- abi/bind/base.go
+++ abi/bind/base.go
@@ -129,6 +129,10 @@ func AsyncDeployContract(opts *TransactOpts, handler func(*types.Receipt, error)
 	return c.asyncTransact(opts, nil, append(bytecode, input...), handler)
 }
 
+func (c *BoundContract) PackCall(opts *TransactOpts, method string, params ...interface{}) ([]byte, error) {
+	return c.abi.Pack(method, params...)
+}
+
 // Call invokes the (constant) contract method with params as input values and
 // sets the output to result. The result type might be a single field for simple
 // returns, a slice of interfaces for anonymous returns and a struct for named
@@ -192,14 +196,6 @@ func (c *BoundContract) Transact(opts *TransactOpts, method string, params ...in
 	return c.transact(opts, &c.address, input)
 }
 
-func (c *BoundContract) Pack(opts *TransactOpts, method string, params ...interface{}) ([]byte, error) {
-	input, err := c.abi.Pack(method, params...)
-	if err != nil {
-		return input, err
-	}
-	return input, nil
-}
-
 func (c *BoundContract) AsyncTransact(opts *TransactOpts, handler func(*types.Receipt, error), method string, params ...interface{}) (*types.Transaction, error) {
 	// Otherwise pack up the parameters and invoke the contract
 	input, err := c.abi.Pack(method, params...)
@@ -230,13 +226,13 @@ func (c *BoundContract) transact(opts *TransactOpts, contract *common.Address, i
 }
 
 func (c *BoundContract) asyncTransact(opts *TransactOpts, contract *common.Address, input []byte, handler func(*types.Receipt, error)) (*types.Transaction, error) {
-	signedTx, err := c.generateSignedTx(opts, contract, input)
+	signedTx, err := c.generateSignedTx2(opts, contract, input)
 	if err != nil {
 		return nil, err
 	}
-	if err = c.transactor.AsyncSendTransaction(ensureContext(opts.Context), signedTx, handler); err != nil {
-		return nil, err
-	}
+	//if err = c.transactor.AsyncSendTransaction(ensureContext(opts.Context), signedTx, handler); err != nil {
+	//	return nil, err
+	//}
 	return signedTx, nil
 }
 
@@ -314,6 +310,83 @@ func (c *BoundContract) generateSignedTx(opts *TransactOpts, contract *common.Ad
 		return nil, err
 	}
 	return signedTx, nil
+}
+
+
+func (c *BoundContract) generateSignedTx2(opts *TransactOpts, contract *common.Address, input []byte) (*types.Transaction, error) {
+	var err error
+
+	// Ensure a valid value field and resolve the account nonce
+	value := opts.Value
+	if value == nil {
+		value = new(big.Int)
+	}
+	// generate random Nonce between 0 - 2^250 - 1
+	max := new(big.Int)
+	max.Exp(big.NewInt(2), big.NewInt(250), nil).Sub(max, big.NewInt(1))
+	//Generate cryptographically strong pseudo-random between 0 - max
+	nonce, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		//error handling
+		return nil, fmt.Errorf("failed to generate nonce: %v", err)
+	}
+
+	// Figure out the gas allowance and gas price values
+	gasPrice := opts.GasPrice
+	if gasPrice == nil {
+		// default value
+		gasPrice = big.NewInt(30000000)
+	}
+
+	gasLimit := opts.GasLimit
+	if gasLimit == nil {
+		// Gas estimation cannot succeed without code for method invocations
+		if contract != nil {
+			if code, err := c.transactor.PendingCodeAt(ensureContext(opts.Context), c.address); err != nil {
+				return nil, err
+			} else if len(code) == 0 {
+				return nil, ErrNoCode
+			}
+		}
+		// If the contract surely has code (or code is not needed), we set a default value to the transaction
+		gasLimit = big.NewInt(30000000)
+	}
+
+	var blockLimit *big.Int
+	blockLimit, err = c.transactor.GetBlockLimit(ensureContext(opts.Context))
+	if err != nil {
+		return nil, err
+	}
+
+	var chainID *big.Int
+	chainID, err = c.transactor.GetChainID(ensureContext(opts.Context))
+	if err != nil {
+		return nil, err
+	}
+
+	var groupID *big.Int
+	groupID = c.transactor.GetGroupID()
+	if groupID == nil {
+		return nil, fmt.Errorf("failed to get the group ID")
+	}
+
+	// Create the transaction, sign it and schedule it for execution
+	var rawTx *types.Transaction
+	str := ""
+	extraData := []byte(str)
+	if contract == nil {
+		rawTx = types.NewContractCreation(nonce, value, gasLimit, gasPrice, blockLimit, input, chainID, groupID, extraData, c.transactor.SMCrypto())
+	} else {
+		rawTx = types.NewTransaction(nonce, c.address, value, gasLimit, gasPrice, blockLimit, input, chainID, groupID, extraData, c.transactor.SMCrypto())
+	}
+	//if opts.Signer == nil {
+	//	return nil, errors.New("no signer to authorize the transaction with")
+	//}
+	//signedTx, err := opts.Signer(types.HomesteadSigner{}, opts.From, rawTx)
+	//if err != nil {
+	//	return nil, err
+	//}
+	return rawTx, nil
 }
 
 // FilterLogs filters contract logs for past blocks, returning the necessary
